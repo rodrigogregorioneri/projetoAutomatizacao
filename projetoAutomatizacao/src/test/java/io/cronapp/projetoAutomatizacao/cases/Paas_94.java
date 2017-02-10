11825a5fb905290c115913b40f3906ca3e5ab1c6
tiago.ramos <tiago.ramos@.techne.com.br>
2017-02-10 11:18:54 -0200

    atualizando git

---
+++ projetoAutomatizacao/src/test/java/io/cronapp/projetoAutomatizacao/cases/Paas_94.java
@@ -0,0 +1,34 @@
+package io.cronapp.projetoAutomatizacao.cases;
+
+import org.junit.Test;
+
+import io.cronapp.projetoAutomatizacao.pages.Login;
+import io.cronapp.projetoAutomatizacao.pages.MenuSuperiorIcone;
+import io.cronapp.projetoAutomatizacao.util.GerenciamentoDriver;
+
+/**
+ * @author tiago.ramos
+ *
+ * Class Paas_94.java
+ * Responsavel por:
+ * 
+ */
+
+public class Paas_94 extends GerenciamentoDriver{
+	
+	Login login = new Login(driver);
+	MenuSuperiorIcone menu = new MenuSuperiorIcone(driver);
+	
+	//@Test
+	public void paas_94(){
+		System.out.println("Caso de teste > Paas_94");
+		login.logar();
+		login.esperaComponente(15);
+		menu.btnNovoProjeto();
+		System.out.println("Caso de teste > Paas_94");
+	}
+	
+	
+
+
+}
